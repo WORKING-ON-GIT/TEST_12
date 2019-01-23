67f908bc55f174d72fa565b3bcca3a597f86327e
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-01-23 15:10:01 +0330

    Making some changes (2)

--- WindowsFormsApp7/Program.cs
+++ WindowsFormsApp7/Program.cs
@@ -8,7 +8,9 @@
 			System.Windows.Forms.Application.EnableVisualStyles();
 			System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
 
-			System.Windows.Forms.Application.Run(new Form1());
+			Form1 form = new Form1();
+
+			System.Windows.Forms.Application.Run(form);
 		}
 	}
 }
