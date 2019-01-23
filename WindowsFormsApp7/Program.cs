15d1496dd9e1feb23dfc9ef3b9f319e2a74b942a
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-01-23 15:09:29 +0330

    Making some changes (1)

--- WindowsFormsApp7/Program.cs
+++ WindowsFormsApp7/Program.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace WindowsFormsApp7
+﻿namespace WindowsFormsApp7
 {
 	static class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
+		[System.STAThread]
 		static void Main()
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			System.Windows.Forms.Application.EnableVisualStyles();
+			System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+
+			System.Windows.Forms.Application.Run(new Form1());
 		}
 	}
 }
